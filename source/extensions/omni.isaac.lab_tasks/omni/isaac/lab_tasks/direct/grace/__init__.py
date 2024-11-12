373160092086e2dd9582026b1a79d3c5f989b0b2
drfly <alessio.mosca@supsi.ch>
2024-11-12 14:35:51 +0100

    modificato conf PPO (pos_rsl_rl_ppo) e episode_length_s (pos_grace_env_cfg)

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/grace/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/grace/__init__.py
@@ -47,7 +47,7 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.pos_grace_env_cfg:GraceFlatEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_flat_ppo_cfg.yaml",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:GraceFlatPPORunnerCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.pos_rsl_rl_ppo_cfg:GraceFlatPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
     },
 )
@@ -59,7 +59,7 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.pos_grace_env:GraceRoughEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_rough_ppo_cfg.yaml",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:GraceRoughPPORunnerCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.pos_rsl_rl_ppo_cfg:GraceRoughPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
     },
 )
--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/grace/agents/pos_rsl_rl_ppo_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/grace/agents/pos_rsl_rl_ppo_cfg.py
@@ -11,68 +11,10 @@ from omni.isaac.lab_tasks.utils.wrappers.rsl_rl import (
     RslRlPpoAlgorithmCfg,
 )
 
-
-@configclass
-class GraceFlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
-    num_steps_per_env = 24
-    max_iterations = 500
-    save_interval = 50
-    experiment_name = "grace_flat_direct"
-    empirical_normalization = False
-    policy = RslRlPpoActorCriticCfg(
-        init_noise_std=1.0,
-        actor_hidden_dims=[128, 128, 128],
-        critic_hidden_dims=[128, 128, 128],
-        activation="elu",
-    )
-    algorithm = RslRlPpoAlgorithmCfg(
-        value_loss_coef=1.0,
-        use_clipped_value_loss=True,
-        clip_param=0.2,
-        entropy_coef=0.005,
-        num_learning_epochs=5,
-        num_mini_batches=4,
-        learning_rate=1.0e-3,
-        schedule="adaptive",
-        gamma=0.99,
-        lam=0.95,
-        desired_kl=0.01,
-        max_grad_norm=1.0,
-    )
-
-
-@configclass
-class GraceRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
-    num_steps_per_env = 24
-    max_iterations = 1500
-    save_interval = 50
-    experiment_name = "grace_rough_direct"
-    empirical_normalization = False
-    policy = RslRlPpoActorCriticCfg(
-        init_noise_std=1.0,
-        actor_hidden_dims=[512, 256, 128],
-        critic_hidden_dims=[512, 256, 128],
-        activation="elu",
-    )
-    algorithm = RslRlPpoAlgorithmCfg(
-        value_loss_coef=1.0,
-        use_clipped_value_loss=True,
-        clip_param=0.2,
-        entropy_coef=0.005,
-        num_learning_epochs=5,
-        num_mini_batches=4,
-        learning_rate=1.0e-3,
-        schedule="adaptive",
-        gamma=0.99,
-        lam=0.95,
-        desired_kl=0.01,
-        max_grad_norm=1.0,
-    )
-
 @configclass
 class GraceFlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 48
-    max_iterations = 200
+    max_iterations = 2000
     save_interval = 50
     experiment_name = "pos_grace_flat_direct"
     empirical_normalization = False
