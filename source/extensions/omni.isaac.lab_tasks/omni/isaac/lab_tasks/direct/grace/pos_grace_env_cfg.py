8b6fad71feed061403ad60c7e519b159b5b6f56b
drfly <alessio.mosca@supsi.ch>
2025-02-11 12:54:11 +0100

    non usa vacuum

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/grace/pos_grace_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/grace/pos_grace_env_cfg.py
@@ -184,7 +184,6 @@ class PosGraceFlatEnvCfg(DirectRLEnvCfg):
                 scale=(0.2, 0.2, 0.8),
                 visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.2, 0.2, 0.8)),
             ),
-            ),
             "gravity": sim_utils.UsdFileCfg(
                 usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/UIElements/arrow_x.usd",
                 scale=(0.2, 0.2, 0.8),
